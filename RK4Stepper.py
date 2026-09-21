1a0bc8bc1535d67af0b4deb8f2ba920aff62b456
krystoham <krystof.ham@volny.cz>
2026-09-21 16:44:32 +0200

    Meni se asteroid velocity

--- RK4Stepper.py
+++ RK4Stepper.py
@@ -17,11 +17,11 @@ class RK4Stepper:
 		problem.getDerivatives(t_0 + t_step/2, state + t_step * self.k2/2,  self.k3)
 		problem.getDerivatives(t_0 + t_step, state + t_step * self.k3,  self.k4)
 
-
+		state_new = state + 1/6*t_step*(self.k1 + 2* self.k2 + 2*self.k3 + self.k4)
 		# earthPosNew = earthPos + 1/6*(t_step)*(self.k1+2*self.k2+2*self.k3+self.k4)
 
 
 		# Fill in this method. It should return new system state after one state.
 		# Use RK4
 
-		return SystemState()
+		return SystemState(state_new.EarthPosition, state_new.EarthPosition, state_new.AsteroidPosition, state_new.AsteroidVelocity)
